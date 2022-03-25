1df4fce108b94e8cc6a0649bcef32fa1d0a51841
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2022-03-25 17:59:44 +0100

    added docker-compose launch to playbook

--- production/node6/run.sh
+++ production/node6/run.sh
@@ -2,4 +2,4 @@
 source "/opt/ros/noetic/setup.bash" && source "/root/catkin_ws/devel/setup.bash"
 export ROS_MASTER_URI=http://192.168.1.235:11311 #intel_nuc-server
 export ROS_IP="$(hostname -I | cut -f1 -d' ')"
-roslaunch firos_lidar_bringup lidar_bringup.launch x:=1 y:=10 z:=0 alpha:=1.57 beta:=0 gama:=0 node_name:=node_6
+roslaunch firos_lidar_bringup lidar_bringup.launch x:=12.39 y:=33.14 z:=0 alpha:=3.14 beta:=0 gama:=0 node_name:=node_6
